c156eec0e46982e235ce2ec0fb92dbeb80513321
Zoshsgahdnkc <2987901713@qq.com>
2022-07-18 12:08:13 +0800

    设置新的摇酒壶jei界面（待测试）

--- src/main/java/io/github/tt432/kitchenkarrot/dependencies/jei/category/CocktailRecipeCategory.java
+++ src/main/java/io/github/tt432/kitchenkarrot/dependencies/jei/category/CocktailRecipeCategory.java
@@ -17,24 +17,24 @@ import net.minecraft.world.item.ItemStack;
  **/
 public class CocktailRecipeCategory extends BaseRecipeCategory<CocktailRecipe> {
     protected static final ResourceLocation BACKGROUND =
-            new ResourceLocation(Kitchenkarrot.MOD_ID, "textures/gui/shaker.png");
+            new ResourceLocation(Kitchenkarrot.MOD_ID, "textures/gui/jei.png");
 
     public CocktailRecipeCategory(IGuiHelper helper) {
         super(JeiPlugin.COCKTAIL,
                 helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModItems.SHAKER.get())),
-                helper.createDrawable(BACKGROUND, 0, 0, 176, 166 - 90));
+                helper.createDrawable(BACKGROUND, 0, 0, 119, 71));
     }
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, CocktailRecipe recipe, IFocusGroup focuses) {
         var ingredients = recipe.getContent().getRecipe();
 
-        builder.addSlot(RecipeIngredientRole.INPUT, 110 + 1, 25 + 1).addIngredients(ingredients.get(0));
-        builder.addSlot(RecipeIngredientRole.INPUT, 128 + 1, 25 + 1).addIngredients(ingredients.get(1));
-        builder.addSlot(RecipeIngredientRole.INPUT, 146 + 1, 25 + 1).addIngredients(ingredients.get(2));
-        builder.addSlot(RecipeIngredientRole.INPUT, 118 + 1, 43 + 1).addIngredients(ingredients.get(3));
-        builder.addSlot(RecipeIngredientRole.INPUT, 136 + 1, 43 + 1).addIngredients(ingredients.get(4));
+        builder.addSlot(RecipeIngredientRole.INPUT, 49, 9).addIngredients(ingredients.get(0));
+        builder.addSlot(RecipeIngredientRole.INPUT, 71, 9).addIngredients(ingredients.get(1));
+        builder.addSlot(RecipeIngredientRole.INPUT, 93, 9).addIngredients(ingredients.get(2));
+        builder.addSlot(RecipeIngredientRole.INPUT, 61, 32).addIngredients(ingredients.get(3));
+        builder.addSlot(RecipeIngredientRole.INPUT, 83, 32).addIngredients(ingredients.get(4));
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 71 + 1, 33 + 1).addItemStack(recipe.getResultItem());
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 15, 32).addItemStack(recipe.getResultItem());
     }
 }

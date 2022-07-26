983f8c28f4129e53149837775d6d94ce7fc50869
glyceryl <494192175@qq.com>
2022-07-26 22:21:19 +0800

    尝试修复鸡尾酒中可能导致空指针异常的bug

--- src/main/java/io/github/tt432/kitchenkarrot/item/CocktailItem.java
+++ src/main/java/io/github/tt432/kitchenkarrot/item/CocktailItem.java
@@ -10,38 +10,43 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author DustW
  **/
+@ParametersAreNonnullByDefault
 public class CocktailItem extends Item {
+
+    public static final ResourceLocation UNKNOWN_COCKTAIL = new ResourceLocation(Kitchenkarrot.MOD_ID, "unknown");
+
     public CocktailItem(Properties properties) {
         super(properties);
     }
 
     @Override
-    public void fillItemCategory(CreativeModeTab p_41391_, NonNullList<ItemStack> p_41392_) {
+    public void fillItemCategory(CreativeModeTab p_41391_, NonNullList<ItemStack> itemStackNonNullList) {
         if (p_41391_ == Kitchenkarrot.COCKTAIL_TAB) {
             for (String cocktail : CocktailList.INSTANCE.cocktails) {
-                var stack = new ItemStack(this);
+                ItemStack stack = new ItemStack(this);
                 setCocktail(stack, new ResourceLocation(cocktail));
-                p_41392_.add(stack);
+                itemStackNonNullList.add(stack);
             }
         }
     }
-
-    public static final ResourceLocation UNKNOWN_COCKTAIL =
-            new ResourceLocation(Kitchenkarrot.MOD_ID, "unknown");
 
     public static ItemStack unknownCocktail() {
         var stack = new ItemStack(ModItems.COCKTAIL.get());
@@ -50,38 +55,34 @@ public class CocktailItem extends Item {
     }
 
     @Override
-    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
-        if (pLivingEntity instanceof ServerPlayer player) {
-            var cocktail = getCocktail(pStack);
-
+    public ItemStack finishUsingItem(ItemStack stack, Level pLevel, LivingEntity livingEntity) {
+        if (livingEntity instanceof ServerPlayer player) {
+            ResourceLocation cocktail = getCocktail(stack);
             if (cocktail != null) {
-                var recipe = get(pLevel, cocktail);
-
+                CocktailRecipe recipe = get(pLevel, cocktail);
                 if (recipe != null) {
                     player.getFoodData().eat(recipe.content.hunger, recipe.content.saturation);
-
                     if (!player.getAbilities().instabuild) {
-                        pStack.shrink(1);
+                        stack.shrink(1);
                     }
 
                     for (EffectStack effectStack : recipe.content.getEffect()) {
                         player.addEffect(effectStack.get());
                     }
                 } else if (cocktail.equals(UNKNOWN_COCKTAIL)) {
-                    var array = ForgeRegistries.MOB_EFFECTS.getValues().stream()
-                            .filter(eff -> "minecraft".equals(eff.getRegistryName().getNamespace()) &&
+                    List<MobEffect> array = ForgeRegistries.MOB_EFFECTS.getValues().stream()
+                            .filter(eff -> "minecraft".equals(Objects.requireNonNull(eff.getRegistryName()).getNamespace()) &&
                                     eff != MobEffects.HEAL && eff != MobEffects.HARM &&
-                                    eff != MobEffects.BAD_OMEN && eff != MobEffects.HERO_OF_THE_VILLAGE).collect(Collectors.toList());
+                                    eff != MobEffects.BAD_OMEN && eff != MobEffects.HERO_OF_THE_VILLAGE).toList();
                     player.addEffect(new MobEffectInstance(array.get(pLevel.random.nextInt(array.size())), 20 * 5, 0));
-
                     if (!player.getAbilities().instabuild) {
-                        pStack.shrink(1);
+                        stack.shrink(1);
                     }
                 }
             }
         }
 
-        return pStack;
+        return stack;
     }
 
     @Override
@@ -90,27 +91,24 @@ public class CocktailItem extends Item {
     }
 
     @Override
-    public String getDescriptionId(ItemStack p_41455_) {
-        var name = getCocktail(p_41455_);
+    public String getDescriptionId(ItemStack stack) {
+        ResourceLocation name = getCocktail(stack);
 
         if (name != null) {
             return name.toString().replace(":", ".");
         }
 
-        return super.getDescriptionId(p_41455_);
+        return super.getDescriptionId(stack);
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, @org.jetbrains.annotations.Nullable Level level, List<Component> p_41423_, TooltipFlag p_41424_) {
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
         ResourceLocation name = getCocktail(stack);
-
-        if (name != null) {
-            p_41423_.add(new TranslatableComponent(name.toString().replace(":", ".") + ".tooltip"));
-
-            var recipe = get(level, getCocktail(stack));
-
+        if (name != null && level != null) {
+            tooltip.add(new TranslatableComponent(name.toString().replace(":", ".") + ".tooltip"));
+            CocktailRecipe recipe = get(level, Objects.requireNonNull(getCocktail(stack)));
             if (recipe != null) {
-                p_41423_.add(new TranslatableComponent("item.cocktail.author", recipe.author));
+                tooltip.add(new TranslatableComponent("item.cocktail.author", recipe.author));
             }
         }
     }
@@ -130,7 +128,7 @@ public class CocktailItem extends Item {
 
     @Nullable
     public static CocktailRecipe get(Level level, ResourceLocation resourceLocation) {
-        var result = level.getRecipeManager().byKey(resourceLocation);
+        Optional<? extends Recipe<?>> result = level.getRecipeManager().byKey(resourceLocation);
 
         if (result.isPresent() && result.get().getType() == RecipeTypes.COCKTAIL.get()) {
             return (CocktailRecipe) result.get();
@@ -138,4 +136,5 @@ public class CocktailItem extends Item {
 
         return null;
     }
+
 }

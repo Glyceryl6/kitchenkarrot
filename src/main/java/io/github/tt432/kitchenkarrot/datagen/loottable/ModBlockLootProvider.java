1c4bb8aa977def022f7196087030aa4e92772330
skyinr <2587135207@qq.com>
2022-11-05 15:05:59 +0800

    dg修复

--- src/main/java/io/github/tt432/kitchenkarrot/datagen/loottable/ModBlockLootProvider.java
+++ src/main/java/io/github/tt432/kitchenkarrot/datagen/loottable/ModBlockLootProvider.java
@@ -4,18 +4,44 @@ import io.github.tt432.kitchenkarrot.block.ModBlocks;
 import io.github.tt432.kitchenkarrot.item.ModBlockItems;
 import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.loot.BlockLoot;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ModBlockLootProvider extends BlockLoot {
 
+    private final Set<Block> skipBlocks = new HashSet<>();
+
+    @Override
+    protected void add(@NotNull Block pBlock, @NotNull LootTable.Builder pLootTableBuilder) {
+        super.add(pBlock, pLootTableBuilder);
+        skipBlocks.add(pBlock);
+    }
+
     @Override
     protected void addTables() {
-        createCropDrops(ModBlocks.GEM_CARROT.get(), ModBlockItems.GEM_CARROT.get(), ModBlockItems.GEM_CARROT.get(),
-                LootItemBlockStatePropertyCondition
-                        .hasBlockStateProperties(Blocks.WHEAT)
-                        .setProperties(StatePropertiesPredicate.Builder.properties()
-                                .hasProperty(CropBlock.AGE, 7)));
+        add(ModBlocks.GEM_CARROT.get(),
+                createCropDrops(ModBlocks.GEM_CARROT.get(),
+                        ModBlockItems.GEM_CARROT.get(),
+                        ModBlockItems.GEM_CARROT.get(),
+                        LootItemBlockStatePropertyCondition
+                                .hasBlockStateProperties(ModBlocks.GEM_CARROT.get())
+                                .setProperties(StatePropertiesPredicate.Builder.properties()
+                                        .hasProperty(CropBlock.AGE, 7))));
     }
+
+    @Override
+    @NotNull
+    protected Iterable<Block> getKnownBlocks() {
+        return skipBlocks;
+    }
+
 }
--- src/main/java/io/github/tt432/kitchenkarrot/datagen/loottable/ModLootTableProvider.java
+++ src/main/java/io/github/tt432/kitchenkarrot/datagen/loottable/ModLootTableProvider.java
@@ -7,11 +7,13 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -33,5 +35,10 @@ public class ModLootTableProvider extends LootTableProvider {
         ImmutableList.Builder<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootContextParamSet>> builder = new ImmutableList.Builder<>();
         builder.add(Pair.of(ModBlockLootProvider::new, LootContextParamSets.BLOCK));
         return builder.build();
+    }
+
+    @Override
+    protected void validate(@NotNull Map<ResourceLocation, LootTable> map, @NotNull ValidationContext validationtracker) {
+
     }
 }
